9bb8a680cbb06c282e7c5eec1b46b78242f22894
Seytani <jessy_lee12@hotmail.com>
2020-09-14 00:12:42 -0400

    workind. did some styling. missing zomato component

--- jam72proj/src/App.tsx
+++ jam72proj/src/App.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
+import Grid from '@material-ui/core/Grid';
 import OpenWeatherMain from './OpenWeather/OpenWeatherMain';
 import Zomato from "./Zomato/Zomato";
+import NasaMain from './Nasa/NasaMain';
+
 
 export interface AppProps { }
 export interface AppState {
@@ -37,20 +40,29 @@ class App extends React.Component<{}, AppState> {
     this.setState({ latitude: pos.coords.latitude })
     this.setState({ longitude: pos.coords.longitude })
   }
-    failure = (pos: PositionError): void => {
-      console.log('error', pos);
-    }
+  failure = (pos: PositionError): void => {
+    console.log('error', pos);
+  }
 
-    render() {
-      const zomatoURL = `https://developers.zomato.com/api/v2.1/geocode?lat=${this.state.latitude}&lon=${this.state.longitude}`;
-      return (
-        <div>
-          {this.state.latitude > 0 && this.state.longitude ?
-            <OpenWeatherMain url={`http://api.openweathermap.org/data/2.5/weather?lat=${this.state.latitude}&lon=${this.state.longitude}&appid=fd18b03e8677888ed6de2e157e4e2db6&units=imperial`} />
-            : null}
-          <Zomato url={zomatoURL} />
-          </div>
-        )
+  render() {
+    const zomatoURL = `https://developers.zomato.com/api/v2.1/geocode?lat=${this.state.latitude}&lon=${this.state.longitude}`;
+    return (
+      <div id='app'>
+        {this.state.latitude > 0 && this.state.longitude ?
+          <Grid container justify='center'>
+              <h1> JAM info </h1>
+              <Grid item xl={12} >
+            <div id='mainTopSection'>
+                <OpenWeatherMain url={`http://api.openweathermap.org/data/2.5/weather?lat=${this.state.latitude}&lon=${this.state.longitude}&appid=fd18b03e8677888ed6de2e157e4e2db6&units=imperial`} />
+                <NasaMain latitude={this.state.latitude} longitude={this.state.longitude} date={this.state.date} />
+            </div>
+              </Grid>
+            <Zomato url={zomatoURL} />
+          </Grid>
+          : null}
+      </div>
+
+    )
   }
 }
 export default App;

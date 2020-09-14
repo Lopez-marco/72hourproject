7b76dd380e0ee45efb6fd38086995c44fe493fce
Seytani <jessy_lee12@hotmail.com>
2020-09-13 20:59:18 -0400

    Updated props

--- jam72proj/src/App.tsx
+++ jam72proj/src/App.tsx
@@ -5,6 +5,7 @@ export interface AppProps {
 export interface AppState {
   latitude: number;
   longitude: number;
+  date: Date;
 }
 
 class App extends React.Component<AppProps, AppState> {
@@ -12,7 +13,8 @@ class App extends React.Component<AppProps, AppState> {
     super(props);
     this.state = {
       latitude: 0,
-      longitude: 0
+      longitude: 0,
+      date: new Date()
     };
     this.getLocation = this.getLocation.bind(this);
     this.success = this.success.bind(this);
@@ -30,16 +32,21 @@ class App extends React.Component<AppProps, AppState> {
       console.log('this is running')
     }
   }
-  success = (pos:Position):void => {
-    this.setState({latitude: pos.coords.latitude})
-    this.setState({longitude: pos.coords.longitude})
+  success = (pos: Position): void => {
+    this.setState({ latitude: pos.coords.latitude })
+    this.setState({ longitude: pos.coords.longitude })
+
   }
 
-  failure = (pos:PositionError):void => {
+  failure = (pos: PositionError): void => {
     console.log('error', pos);
   }
   render() {
-  return (<div>{this.state.latitude} {this.state.longitude}</div>);
+    return (
+      <div>
+        {this.state.latitude > 0 && this.state.longitude > 0 ? <p>Add your component here</p> : null}
+      </div>
+    );
   }
 }
 

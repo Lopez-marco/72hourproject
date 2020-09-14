7d1de1b9ccaa49ea556b0abd3cadc9833e27737f
lopez-marco <lopez.marcohdez@gmail.com>
2020-09-13 22:09:01 -0400

    72Hourdevelop

--- jam72proj/src/App.tsx
+++ jam72proj/src/App.tsx
@@ -1,7 +1,7 @@
-import React from 'react';
+import React from "react";
+import Zomato from "./Zomato/Zomato";
 
-export interface AppProps {
-}
+export interface AppProps {}
 export interface AppState {
   latitude: number;
   longitude: number;
@@ -12,7 +12,7 @@ class App extends React.Component<AppProps, AppState> {
     super(props);
     this.state = {
       latitude: 0,
-      longitude: 0
+      longitude: 0,
     };
     this.getLocation = this.getLocation.bind(this);
     this.success = this.success.bind(this);
@@ -24,22 +24,30 @@ class App extends React.Component<AppProps, AppState> {
   }
 
   getLocation() {
-    console.log('this runs');
+    console.log("this runs");
     if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(this.success, this.failure)
-      console.log('this is running')
+      navigator.geolocation.getCurrentPosition(this.success, this.failure);
+      console.log("this is running");
     }
   }
-  success = (pos:Position):void => {
-    this.setState({latitude: pos.coords.latitude})
-    this.setState({longitude: pos.coords.longitude})
-  }
+  success = (pos: Position): void => {
+    this.setState({ latitude: pos.coords.latitude });
+    this.setState({ longitude: pos.coords.longitude });
+  };
 
-  failure = (pos:PositionError):void => {
-    console.log('error', pos);
-  }
+  failure = (pos: PositionError): void => {
+    console.log("error", pos);
+  };
+
   render() {
-  return (<div>{this.state.latitude} {this.state.longitude}</div>);
+    const zomatoURL = `https://developers.zomato.com/api/v2.1/geocode?lat=${this.state.latitude}&lon=${this.state.longitude}`;
+    console.log(zomatoURL);
+    return (
+      <div>
+        {this.state.latitude} {this.state.longitude}
+        <Zomato url={zomatoURL} />
+      </div>
+    );
   }
 }
 
